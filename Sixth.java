c2b3ba158268857cc1af874996cb1721c796febc
viswpriya0505 <viswpriya0505@gmail.com>
2025-08-22 18:12:38 +0530

    Modified

--- Sixth.java
+++ Sixth.java
@@ -1,12 +1,13 @@
 Class Sixth{
 
-
-public void setId(int id){
-this.id=id;
-
   private int id;
 
   public int getId(){
     return this.id;
+  }
+
+  public void setId(int id){
+     this.id=id;
+  }
 
 }
