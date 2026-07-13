3ee14d790febfa6c8d0f3b1d131edd4f74da9ada
viswpriya0505 <viswpriya0505@gmail.com>
2026-07-13 19:21:54 +0530

    modified-sixth.java

--- Sixth.java
+++ Sixth.java
@@ -2,13 +2,24 @@ Class Sixth{
 
   private int id;
 
+  private String num;
+
   public int getId(){
     return this.id;
+  }
+
+  public String getNum(){
+    return this.num;
   }
 
   public void setId(int id){
      this.id=id;
   }
+  
+  public void setName(String name){
+     this.name=name;
+  }
+
 
   public String toString(){
      return "ID is returning";
